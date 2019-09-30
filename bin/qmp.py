8933ecf73f91c36d128e230881269ab74b6f622a
Alexei Colin <acolin@isi.edu>
2019-09-30 12:13:55 -0400

    qmp: tolerate JSON parsing errors due to multiclients
    Also, clear the channel before sending response, instead of after
    reading reply, to maximize chances that there won't be interference
    from other clients in the between our send and receive.

--- bin/qmp.py
+++ bin/qmp.py
@@ -19,15 +19,23 @@ class QMP:
         req = json.dumps({"execute": cmd, "arguments": args})
         if self.verbose:
             print(req)
-        self.cl.write(req.encode())
-        reply = self.cl.read_until(b"\r\n")
-        # some commands print multiple newlines; we must eat them, otherwise
-        # the next request will read them and return immediately.
+
+        # some replies print multiple newlines; we must eat whatever
+        # garbage before beginning the RPC. Also, when other clients
+        # send requests (e.g. GDB continue), the reply gets sent
+        # over our channel too.... no clear way to deal with it, other
+        # than make JSON errors non-fatal (see below)
         try:
             self.cl.read_eager()
         except BlockingIOError:
             pass
+        self.cl.write(req.encode())
+        reply = self.cl.read_until(b"\r\n")
         reply = reply.decode()
         if self.verbose:
             print(reply)
-        return json.loads(reply)
+        try:
+            return json.loads(reply)
+        except json.decoder.JSONDecodeError as e:
+            print("ERROR: failed to parse JSON:", e, file=sys.stderr)
+            print("  Bad JSON input:", reply)
