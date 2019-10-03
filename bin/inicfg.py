889e14e8b6ac9d0a4f078f093ee8480c061ed5a8
Alexei Colin <acolin@isi.edu>
2019-10-03 12:29:44 -0400

    inicfg: move IEC size parser to here, add helpers

--- bin/inicfg.py
+++ bin/inicfg.py
@@ -1,6 +1,28 @@
 # Shared utilities for working with config files in INI format
 
 import configparser
+import re
+
+def int_autobase(val):
+    if val.startswith("0x"):
+        return int(val, 16)
+    if re.match(r'0[0-9]+', val):
+        return int(val, 8)
+    return int(val, 10)
+
+def size_from_iec_str(s):
+    m = re.match(r'((?:0x)?(?:[0-9]+))([KMG]?)', s)
+    if not m:
+        raise IniCfgException("size not in IEC format: '%s'" % s)
+    size = int_autobase(m.group(1))
+    suffix = m.group(2)
+    if suffix == 'G':
+        size <<= 30
+    if suffix == 'M':
+        size <<= 20
+    if suffix == 'K':
+        size <<= 10
+    return size
 
 class IniCfg:
     """Helper for working with configuration in INI format"""
@@ -65,6 +87,26 @@ class IniCfg:
         if prop in scfg:
             return scfg[prop]
         return fallback
+
+    def get_prop_required(self, sect, prop):
+        scfg = self.get_collapsed(sect)
+        if prop in scfg:
+            return scfg[prop]
+        raise IniCfg.CfgException(
+            "ERROR: section %s has no property %s'" % (sect, prop))
+
+    def get_prop_as(self, sect, prop, ctor):
+        val = self.get_prop_required(sect, prop)
+        return ctor(val)
+
+    def get_prop_as_int(self, sect, prop):
+        return self.get_prop_as(sect, prop, int_autobase)
+
+    def get_prop_as_bool(self, sect, prop):
+        return self.get_prop_as(sect, prop, bool)
+
+    def get_prop_as_size(self, sect, prop):
+        return self.get_prop_as(sect, prop, size_from_iec_str)
 
     def sections(self):
         return self.base_sections
