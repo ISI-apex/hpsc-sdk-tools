ba522782af2e21fce185f2c10b06ee6addad4aa8
Alexei Colin <acolin@isi.edu>
2019-10-15 17:38:20 -0400

    memmap: support blob slices

--- bin/memmap.py
+++ bin/memmap.py
@@ -3,6 +3,8 @@
 import re
 import os
 
+from numparse import int_autobase, size_from_iec_str
+
 class MemMap:
     # Special values of destination/address field
     DEST_ELF = '@ELF'
@@ -10,6 +12,12 @@ class MemMap:
 
     class MemMapException(Exception):
         pass
+
+    class Blob:
+        def __init__(self, fname, slice_offset, slice_length):
+            self.fname = fname
+            self.slice_offset = slice_offset
+            self.slice_length = slice_length
 
     def not_comment(line):
         line = re.sub(r'\s*#.*', '', line.strip())
@@ -33,12 +41,19 @@ class MemMap:
             if addr == MemMap.DEST_NULL:
                 continue
             fname = os.path.expandvars(tok[3])
-            yield line_num, m, k, addr, fname
+            slice_offset = size_from_iec_str(tok[4]) if len(tok) > 4 else 0x0
+            slice_length_str = tok[5] if len(tok) > 5 else None
+            if slice_length_str is not None and slice_length_str != '-':
+                slice_length = size_from_iec_str(slice_length_str)
+            else:
+                slice_length = None
+            blob = MemMap.Blob(fname, slice_offset, slice_length)
+            yield line_num, m, k, addr, blob
 
     def iter_mem(fmap, mem):
-        for line, m, k, addr, fname in MemMap.iter(fmap):
+        for line, m, k, addr, blob in MemMap.iter(fmap):
             if m == mem:
-                yield line, k, addr, fname
+                yield line, k, addr, blob
 
     class Rules:
         """ Helpers for generating makefile rules from memory map"""
